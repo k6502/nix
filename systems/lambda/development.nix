b8a210efabc2e701eef38c9d26728bd155259b09
k6502 <dev@kthread.dev>
2026-03-03 00:29:52 -0300

    devtools

--- systems/lambda/development.nix
+++ systems/lambda/development.nix
@@ -2,6 +2,21 @@
 
 {
   environment.systemPackages = [
+    neovim
+    python315FreeThreading
+    page
+    nodejs_25
+    pnpm
+    luajit
+    luajitPackages.luarocks
+    fd
+    ripgrep
+    fzf
+    meson
+    cmake
+    libgccjit
+    libgcc
+    gcc
     (pkgs.fenix.complete.withComponents [
       "cargo"
       "clippy"
