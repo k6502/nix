933f9e97adf92dbd007f001207fd2d5ff4dafa2a
k6502 <dev@kthread.dev>
2026-03-03 01:41:07 -0300

    wish me luck bra

--- systems/lambda/systemd.nix
+++ systems/lambda/systemd.nix
@@ -1,6 +1,12 @@
 { config, pkgs, ... }:
 
 {
+  services.userborn.enable = true;
+  system.etc.overlay = {
+    enable = true;
+    mutable = true;
+  };
+
   boot.initrd.systemd = {
     enable = true;
     tpm2 = {
