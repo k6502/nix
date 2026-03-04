9b4292cc04cc642f87da01d277674a8e31f849ac
k6502 <dev@kthread.dev>
2026-03-04 00:11:51 -0300

    networking

--- systems/lambda/default.nix
+++ systems/lambda/default.nix
@@ -18,21 +18,11 @@
     ./locale.nix
     ./security.nix
     ./steam.nix
+    ./networking.nix
   ];
-
-  networking.hostName = "lambda"; # Define your hostname.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "America/Montevideo";
-
-  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
---
+++ systems/lambda/networking.nix
@@ -0,0 +1,63 @@
+{ pkgs, ... }:
+
+{
+  networking = {
+    hostName = "lambda";
+    tempAddresses = "enabled";
+    search = [ "~." ];
+    networkmanager = {
+      enable = true;
+      dhcp = "internal";
+      dns = "systemd-resolved";
+      ethernet = {
+        macAddress = "random";
+      };
+    };
+    firewall = {
+      enable = true;
+      backend = "nftables";
+      checkReversePath = "strict";
+      autoLoadConntrackHelpers = true;
+      filterForward = true;
+      extraInputRules = ''
+        ct state invalid drop
+        ct state { established, related } accept
+      '';
+    };
+    nftables = {
+      enable = true;
+      flushRuleset = true;
+      checkRuleset = true;
+    };
+    stevenblack = {
+      enable = true;
+      block = [
+        "fakenews"
+        "gambling"
+        "porn"
+        "social"
+      ];
+    };
+  };
+  services = {
+    resolved = {
+      enable = true;
+      settings = {
+        Resolve = {
+          DNS = [
+            "1.1.1.1#cloudflare-dns.com"
+            "1.0.0.1#cloudflare-dns.com"
+            "2606:4700:4700::1111#cloudflare-dns.com"
+            "2606:4700:4700::1001#cloudflare-dns.com"
+          ];
+          Domains = [ "~." ];
+          DNSOverTLS = "yes";
+          DNSSEC = "true";
+        };
+      };
+    };
+  };
+  environment.defaultPackages = with pkgs; [
+    stevenblack-blocklist
+  ];
+}
