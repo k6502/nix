bb1b2665a67a9fc91722c5a7f55361603fd29c9d
k6502 <dev@kthread.dev>
2026-03-05 02:17:11 -0300

    zsh

--- systems/lambda/users.nix
+++ systems/lambda/users.nix
@@ -1,9 +1,56 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
+  programs.zsh = {
+    enable = true;
+    zsh-autoenv = {
+      enable = true;
+    };
+    syntaxHighlighting = {
+      enable = true;
+      highlighters = [
+        "main"
+        "brackets"
+        "pattern"
+        "cursor"
+        "regexp"
+        "root"
+        "line"
+      ];
+    };
+    ohMyZsh = {
+      enable = true;
+      preLoaded = ''
+        zstyle :omz:plugins:ssh-agent agent-forwarding yes
+      '';
+      plugins = [
+        "git"
+        "direnv"
+        "fzf"
+        "git-auto-fetch"
+        "gpg-agent"
+        "ssh-agent"
+        "rust"
+        "ssh"
+      ];
+    };
+    enableLsColors = true;
+    enableGlobalCompInit = true;
+    enableCompletion = true;
+    enableBashCompletion = true;
+    autosuggestions = {
+      enable = true;
+      strategy = [
+        "history"
+        "completion"
+        "match_prev_cmd"
+      ];
+    };
+  };
   users.users.kptr = {
     isNormalUser = true;
     description = "kptr";
+    shell = pkgs.zsh;
     extraGroups = [
       "users"
       "wheel"
