c9300439cea84b3d3d0b7a6102d97579fa033ca1
k6502 <dev@kthread.dev>
2026-03-05 01:27:23 -0300

    some xdg stuff and a wallpaper manager

--- systems/lambda/cosmic.nix
+++ systems/lambda/cosmic.nix
@@ -3,11 +3,42 @@
 {
   environment.defaultPackages = with pkgs; [
     cosmic-wallpapers
+    variety
   ];
   services = {
     displayManager = {
       cosmic-greeter.enable = true;
     };
     desktopManager.cosmic.enable = true;
+  };
+  xdg = {
+    mime = {
+      enable = true;
+    };
+    menus = {
+      enable = true;
+    };
+    icons = {
+      enable = true;
+    };
+    sounds = {
+      enable = true;
+    };
+    portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal-cosmic
+      ];
+    };
+    autostart = {
+      enable = true;
+    };
+    terminal-exec = {
+      enable = true;
+      package = with pkgs; [ alacritty ];
+    };
   };
 }
