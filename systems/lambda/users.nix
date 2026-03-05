b2f5cdeaa205952aa67634c7d68b297508845b2f
k6502 <dev@kthread.dev>
2026-03-05 20:34:39 -0300

    obs

--- systems/lambda/users.nix
+++ systems/lambda/users.nix
@@ -84,4 +84,15 @@
       })
     ];
   };
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vaapi # optional AMD hardware acceleration
+      obs-gstreamer
+      obs-vkcapture
+    ];
+  };
 }
